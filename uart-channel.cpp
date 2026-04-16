1eb317fbd2b13dac31192ab40c16d11b0b515a80
Christopher R. Palmer <crpalmer@gmail.com>
2026-04-16 09:07:24 -0400

    uart-channel: add a checksum

--- uart-channel.cpp
+++ uart-channel.cpp
@@ -4,6 +4,20 @@
 #include "mem.h"
 #include "uart-channel.h"
 
+static const uint32_t MOD_ADLER = 65521;
+
+uint32_t adler32(const void *data_vp, size_t len) {
+    uint32_t a = 1, b = 0;
+    const uint8_t *data = (uint8_t *) data_vp;
+
+    for (size_t index = 0; index < len; index++) {
+        a = (a + data[index]) % MOD_ADLER;
+        b = (b + a) % MOD_ADLER;
+    }
+
+    return (b << 16) | a;
+}
+
 UARTChannel::UARTChannel(int tx_pin, int rx_pin, const char *name) : PiThread(name) {
     tx = pico_new_uart_tx(tx_pin);
     rx = pico_new_uart_rx(rx_pin);
@@ -27,8 +41,16 @@ void UARTChannel::main(void) {
 	if (! read_int(&cmd) || ! read_int(&n_data) || n_data < 0) continue;
 
 	if (n_data > 0) {
+	    uint32_t checksum;
 	    data = fatal_malloc(n_data);
 	    rx->read(data, n_data);
+	    rx->read(&checksum, sizeof(checksum));
+	    uint32_t received_checksum = adler32(data, n_data);
+	    if (received_checksum != checksum) {
+		printf("uart-channel: invalid checksum %x != %x\n", (unsigned) checksum, (unsigned) received_checksum);
+		fatal_free(data);
+		continue;
+	    }
 	}
 
 	on_command(cmd, data, n_data);
@@ -43,7 +65,11 @@ void UARTChannel::send_command(int cmd, const void *data, int n_data) {
     tx->putc(0x7f);
     write_int(cmd);
     write_int(n_data);
-    tx->write(data, n_data);
+    if (n_data) {
+	uint32_t checksum = adler32(data, n_data);
+	tx->write(data, n_data);
+	tx->write(&checksum, sizeof(checksum));
+    }
 }
 
 void UARTChannel::write_int(int value) {
